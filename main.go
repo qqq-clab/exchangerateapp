cf3626fff5e2426075d3d1702dcb93b1945434e1
qqq-clab <wujianhan@LAPTOP-U9SR08IL>
2026-01-19 04:38:07 +0800

    First step to use gin

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"exchangeapp/config"
-	"fmt"
+	// "fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.InitConfig()
-	fmt.Println(config.AppConfig.App.Port)
-
+	// fmt.Println(config.AppConfig.App.Port)
+	 r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+	c.JSON(200, gin.H{
+	"message": "pong",
+	})
+	})
+	r.Run()
 }
