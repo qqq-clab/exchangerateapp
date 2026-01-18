5624790746302726a6e192b87774b4f0ea9d69b8
qqq-clab <wujianhan@LAPTOP-U9SR08IL>
2026-01-19 04:42:36 +0800

    format clean

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 func main() {
 	config.InitConfig()
 	// fmt.Println(config.AppConfig.App.Port)
-	 r := gin.Default()
+	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-	c.JSON(200, gin.H{
-	"message": "pong",
-	})
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
 	r.Run()
 }
