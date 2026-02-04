96c9111f318d08cf84e2e7e4f8eda00ee7fc794d
qqq-clab <wujianhan@LAPTOP-U9SR08IL>
2026-02-05 00:02:06 +0800

    现在ctrl+c退出应用时会等活跃的http请求5秒后关闭

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
 	"exchangeapp/config"
 	"exchangeapp/router"
 	// "fmt"
@@ -10,5 +17,34 @@ func main() {
 	config.InitConfig()
 	// fmt.Println(config.AppConfig.App.Port)
 	r := router.SetupRouter()
-	r.Run()
+
+	port := config.AppConfig.App.Port
+
+	if port == "" {
+		port = ":8080"
+	}
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+
 }
